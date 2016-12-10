fb61069886c7ad78163973afe174a1e55ac2984d
Shea Rogers <Shea Rogers>
2016-12-09 22:40:25 -0500

    Fixing Travis

--- conf/karma.conf.js
+++ conf/karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function (config) {
       outputDir: 'test-reports'
     },
     browsers: [
-      'Chrome'
+      'Firefox'
     ],
     frameworks: [
       'jasmine'
@@ -38,6 +38,7 @@ module.exports = function (config) {
       require('karma-junit-reporter'),
       require('karma-coverage'),
       require('karma-chrome-launcher'),
+      require('karma-firefox-launcher'),
       require('karma-webpack')
     ]
   };
